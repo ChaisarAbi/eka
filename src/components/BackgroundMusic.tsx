624435a45e244c67d0a837abe8f159b2a7f5193b
ChaisarAbi <chaisarabi2304@gmail.com>
2026-06-14 15:57:18 +0700

    fix: ganti audio ke file lokal canon.mp3

--- src/components/BackgroundMusic.tsx
+++ src/components/BackgroundMusic.tsx
@@ -12,9 +12,7 @@ export default function BackgroundMusic() {
   const hasInteracted = useRef(false);
 
   useEffect(() => {
-    audioRef.current = new Audio(
-      "https://rr4---sn-npoldnez.googlevideo.com/videoplayback?expire=1780606753&ei=wZIhaoi4N_b8-LAPj-KzyQc&ip=181.115.161.87&id=o-AGjlNC4wIuQZC98NifCnR3MSnysyTqcpTYTwxf-BrFDH&itag=251&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&cps=518&bui=AbKmrwo9vznsVlVWHVSS6huVO6yLZQhhQUC8ixcq4OZRj3JiSKh-jXuBZPKiqaJkLu7dUzra0xGZ-EI6&vprv=1&svpuc=1&mime=audio%2Fwebm&rqh=1&gir=yes&clen=3104074&dur=170.581&lmt=1774729524678776&keepalive=yes&fexp=51565115,51565682&c=ANDROID_VR&txp=5532534&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cbui%2Cvprv%2Csvpuc%2Cmime%2Crqh%2Cgir%2Cclen%2Cdur%2Clmt&sig=AHEqNM4wRQIgfrB_cwfFEJ2Mnf9YTWp-qlnyoz_cIFEgo024JSBBKGQCIQDFIjMFgQ--5gO0T90WmaI5nERB4tLwKCtMVEBctNX39Q%3D%3D&rm=sn-xouxacv-a2cz7s,sn-njaee7z&rrc=79,104&req_id=2a9c49947697a3ee&cmsv=e&rms=rdu,aub&redirect_counter=2&cms_redirect=yes&ipbypass=yes&met=1780598283,&mh=eC&mip=103.18.34.149&mm=29&mn=sn-npoldnez&ms=rdu&mt=1780597743&mv=m&mvi=4&pl=24&lsparams=cps,ipbypass,met,mh,mip,mm,mn,ms,mv,mvi,pl,rms&lsig=APaTxxMwRQIhAMi8JdnZevTu8OFTxhY-0eQS0kjXlF7LNumSEgsP4d3VAiAp0MDlxO5UT_VQPuHFL2nTTBJxVyyDK9X4fZUq_XBB-Q%3D%3D"
-    );
+    audioRef.current = new Audio("/music/canon.mp3");
     audioRef.current.loop = true;
     audioRef.current.volume = 0.3;
 
